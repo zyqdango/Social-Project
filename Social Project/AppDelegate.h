94829868c44dd70d2b6cfe26de39f1f5fd829b76
YONGQING ZHENG <yongqingzheng@YONGQINGdeiMac.local>
2017-08-17 23:35:43 -0400

    Initial Commit

---
+++ Social Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Social Project
+//
+//  Created by YONGQING ZHENG on 2017/8/17.
+//  Copyright © 2017年 YONGQING ZHENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Social Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Social Project
+//
+//  Created by YONGQING ZHENG on 2017/8/17.
+//  Copyright © 2017年 YONGQING ZHENG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
